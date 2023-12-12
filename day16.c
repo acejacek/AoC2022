5785577739f060ea68eaea7beb026dc3e4646972
aceJacek <acejacek@gmail.com>
2023-12-12 22:49:25 +0100

    Day 16 - massive caching and performance improvements

--- day16.c
+++ day16.c
@@ -3,10 +3,26 @@
 #include <string.h>
 #include <limits.h>
 #include <ctype.h>
+#include <stdint.h>
 
 #define MAX_TUNELS 5
+#define TIME 30
 
 #define INPUT_FILE "input/day16.txt"
+
+/* 
+ * I'm not sure I can further optimize this program without total rethink of algorithm
+ * It's still slow. Part 1 is solved in under 1 minute on my thin Linux desktop
+ * but part two takes painfully long time.
+ *
+ * to reduce impact on memory, uncomment #define PART 2:
+ */
+
+//#define PART 2
+
+#if PART == 2
+#define TIME 26
+#endif
 
 typedef struct valve
 {
@@ -16,15 +32,14 @@ typedef struct valve
     struct valve* next_v[MAX_TUNELS];
     int t_num;
     int open;
+    unsigned char valveID;
 } Valve;
 
 typedef struct cache
 {
-    Valve* valve;
     Valve* valve2;
     int score;
-    int time;
-    int valves_status[100];
+    uint64_t valves_status_mask;
     struct cache* next;
 } Cache;
 
@@ -32,9 +47,9 @@ typedef struct
 {
     Valve* valves;
     int count;
-} ValvesSet;
+    Cache* (*cache_head)[TIME];   // weird notation, to have array[][] of pointers to Cache*
 
-Cache* cache_head = NULL;
+} ValvesSet;
 
 Valve* find_valve(ValvesSet* all, char* name)
 {
@@ -44,22 +59,20 @@ Valve* find_valve(ValvesSet* all, char* name)
     return NULL;
 }
 
+// used in part 1
 int visit_valve(Valve* v, ValvesSet* all, int const time)
 {
     if (time == 0) return 0;
 
-    for (Cache* cache = cache_head; cache != NULL; cache = cache->next)
-        if (cache->time == time && cache->valve == v)
-        {
-            int same = 1;
-            for (int i = 0; i < all->count; ++i)
-                if (all->valves[i].open != cache->valves_status[i])
-                {
-                    same = 0;
-                    break;
-                }
-            if (same) return cache->score;
-        }
+    Cache* cache = all->cache_head[v->valveID][time - 1];
+    if (cache)
+    {
+        // calculate mask from currently open valves
+        uint64_t valves_status = 0;
+        for (int i = 0; i < all->count; ++i)
+            valves_status |= ((uint64_t)all->valves[i].open << i);
+        if (valves_status == cache->valves_status_mask) return cache->score;
+    }
 
     int max = 0;
     // this valve can be opened & it makes sense to open it (flow > 0)
@@ -87,41 +100,35 @@ int visit_valve(Valve* v, ValvesSet* all, int const time)
            exit(1);
        }
 
-    new_cache->valve = v;
-    new_cache->time = time;
     new_cache->score = max;
+    new_cache->valves_status_mask = 0;
     for (int i = 0; i < all->count; ++i)
-        new_cache->valves_status[i] = all->valves[i].open;
+        if (all->valves[i].open)
+                new_cache->valves_status_mask |= ((uint64_t)1 << i);
 
-    new_cache->next = cache_head;
-    cache_head = new_cache;
+    new_cache->next = all->cache_head[v->valveID][time - 1];
+    all->cache_head[v->valveID][time - 1] = new_cache;
 
     return max;
 }
 
+// used in part 2
 int visit_2_valves(Valve* v1, Valve* v2, ValvesSet* all, int const time)
 {
     if (time == 0) return 0;
 
-    for (Cache* cache = cache_head; cache != NULL; cache = cache->next)
-        if (
-                cache->time == time &&
-                cache->valve == v1 &&
-                cache->valve2 == v2
-           )
+    for (Cache* cache = all->cache_head[v1->valveID][time - 1]; cache != NULL; cache =cache->next)
+        if (cache->valve2 == v2)
         {
-            int same = 1;
+            uint64_t valves_status = 0;
             for (int i = 0; i < all->count; ++i)
-                if (all->valves[i].open != cache->valves_status[i])
-                {
-                    same = 0;
-                    break;
-                }
-            if (same) return cache->score;
+                valves_status |= ((uint64_t)all->valves[i].open << i);
+
+            if (valves_status == cache->valves_status_mask) return cache->score;
         }
 
     int max = 0;
-    // #1 both open valves
+    // #1 we both open valves
     if (v1->open == 0 && v1->flow != 0 &&
         v2->open == 0 && v2->flow != 0 &&
         v1 != v2)   // can't both open the same valve!
@@ -131,7 +138,7 @@ int visit_2_valves(Valve* v1, Valve* v2, ValvesSet* all, int const time)
         // flow will be counting from next minute, hence -1
         max = v1->flow * (time - 1);
         max += v2->flow * (time - 1);
-        // visit the same valve, now it will be opened
+        // visit the same valves, now they will be opened
         max += visit_2_valves(v1, v2, all, time - 1);
         v1->open = 0;  // close it back
         v2->open = 0;  // close it back
@@ -144,12 +151,14 @@ int visit_2_valves(Valve* v1, Valve* v2, ValvesSet* all, int const time)
         v1->open = 1;
         int temp_max2 = v1->flow * (time - 1);
         int tunel_visit_max = 0;
+        // my valve is open, and elephant goes to his tunnels
         for (int i = 0; i < v2->t_num; ++i)
         {
             int temp_max = visit_2_valves(v1, v2->next_v[i], all, time - 1);
             if (tunel_visit_max < temp_max) tunel_visit_max = temp_max;
         }
         v1->open = 0;
+        // update max, if it was better, than previous scenario
         if (max < temp_max2 + tunel_visit_max) max = temp_max2 + tunel_visit_max;
     }
     // #3 Elephant opens valve and I move forward
@@ -158,6 +167,7 @@ int visit_2_valves(Valve* v1, Valve* v2, ValvesSet* all, int const time)
         v2->open = 1;
         int temp_max2 = v2->flow * (time - 1);
         int tunel_visit_max = 0;
+        // same as above, just change of roles
         for (int i = 0; i < v1->t_num; ++i)
         {
             int temp_max = visit_2_valves(v1->next_v[i], v2, all, time - 1);
@@ -167,7 +177,7 @@ int visit_2_valves(Valve* v1, Valve* v2, ValvesSet* all, int const time)
         if (max < temp_max2 + tunel_visit_max) max = temp_max2 + tunel_visit_max;
     }
 
-    // #4 just visit linked tunnels 
+    // #4 we don't open anything, just visit linked tunnels 
     for (int i = 0; i < v1->t_num; ++i)
         for (int j = 0; j < v2->t_num; ++j)
         {
@@ -182,27 +192,32 @@ int visit_2_valves(Valve* v1, Valve* v2, ValvesSet* all, int const time)
            exit(1);
        }
 
-    new_cache->time = time;
-    new_cache->valve = v1;
+    // save results to cache
     new_cache->valve2 = v2;
     new_cache->score = max;
+    // set bits in mask for all open valves
+    new_cache->valves_status_mask = 0;
     for (int i = 0; i < all->count; ++i)
-        new_cache->valves_status[i] = all->valves[i].open;
+         new_cache->valves_status_mask |= ((uint64_t)all->valves[i].open << i);
 
-    new_cache->next = cache_head;
-    cache_head = new_cache;
+    new_cache->next = all->cache_head[v1->valveID][time - 1];
+    all->cache_head[v1->valveID][time - 1] = new_cache;
 
     return max;
 }
 
-void free_cache(void)
+void free_cache(ValvesSet* all)
 {
-    while (cache_head != NULL)
-    {
-        Cache* tmp = cache_head;
-        cache_head = cache_head->next;
-        free(tmp);
-    }
+    //[TODO] for sure there is a smarter way to deallocate cache memory
+    for (int i = 0; i < TIME; ++i)
+        for (int j = 0; j < all->count; ++j)
+        while (all->cache_head[j][i] != NULL)
+        {
+            Cache* tmp = all->cache_head[j][i];
+            all->cache_head[j][i] = all->cache_head[j][i]->next;
+
+            free(tmp);
+        }
 }
 
 int main(void)
@@ -237,6 +252,7 @@ int main(void)
         valves[v].flow = atoi(line + 23);
         valves[v].open = 0;
         valves[v].t_num = 0;
+        valves[v].valveID = v;       // for better cache
 
         int i = 0;
         char* tunnel = strtok(line + 25, " ,;\n");
@@ -249,7 +265,14 @@ int main(void)
         while ((tunnel = strtok(NULL, " ,;\n")) != NULL);
     }
 
-    ValvesSet all = { valves, v };
+    ValvesSet all = { valves, v, NULL };
+    all.cache_head = malloc(v * sizeof(Cache*[TIME]));
+    if (! all.cache_head)
+    {
+        fprintf(stderr, "Can't allocate memory.");
+        exit_status = EXIT_FAILURE;
+        goto cleanup;
+    }
 
     // create pointer indexes for faster traversing
     for (int i = 0; i < v; ++i)
@@ -259,17 +282,19 @@ int main(void)
     Valve* start = find_valve(&all, "AA");
 
     int pressure; 
-//    pressure = visit_valve(start, &all, 30);
-//    printf("Released pressure: %d\n", pressure);
+#if PART == 1
+    pressure = visit_valve(start, &all, TIME);
+    printf("Released pressure: %d\n", pressure);
 
-    free_cache();
-
+    free_cache(&all);
+#else 
     pressure = visit_2_valves(start, start, &all, 26);
     printf("Released pressure in part 2: %d\n", pressure);
+#endif
 
 cleanup:
     if (input) fclose(input);
-    free_cache();
+    free_cache(&all);
     free(valves);
     exit (exit_status);
 }
